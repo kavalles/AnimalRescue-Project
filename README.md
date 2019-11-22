1e4847be64a62494dcae1d2b0e638c16d279884d
Katelyn Valles <kavalles@asu.edu>
2019-11-21 21:28:59 -0700

    project structure completed

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # cpi310-project
 
 please put your updated code into this repository.
+
+Run via these two commands, one after the other:
+
+npm install
+node --inspect index.js
---
+++ index.js
@@ -0,0 +1,149 @@
+//Katelyn Valles
+//CPI310 - Tu-Th
+
+const sqlite3 = require('sqlite3').verbose();
+const express = require("express");
+var http = require('http');
+var fs = require("fs");
+var app = express();
+
+const bodyParser = require('body-parser')
+
+//new code added
+const exphbs = require("express-handlebars");
+
+var path = require("path");
+//app.use(express.static(path.join(__dirname, '/public')));
+app.use(express.static('public'))
+app.use(express.urlencoded());
+const dbPromise = new sqlite3.Database("./mydb.sqlite3");
+app.use(bodyParser.urlencoded({extended: false}))
+app.set("view engine", "handlebars");
+app.engine("handlebars", exphbs());
+
+//creates a new empty instance of a sqlite database
+var db;
+
+async function createDb()
+{
+	db = await dbPromise;
+	//checks if the database has been created without error
+	console.log('Database created!');
+
+	/* Put code to create tables(s) here */
+	//Read seedScripts.txt file into dbSeedString variable as a string
+	var dbSeedString = fs.readFileSync(path.resolve(__dirname, "./seedScripts.txt"),'utf8');
+	db.exec(dbSeedString);
+	console.log("The seedscript created the database and the values");
+	//db.close();
+	/*db.each("SELECT * from user", function(err,row){
+         console.log(row.id + ": " + row.username);
+			});
+			*/
+}
+
+function stringToHash(string) { 
+	var hash = 0; 
+	if (string.length == 0) return hash; 
+	for (i = 0; i < string.length; i++) { 
+		char = string.charCodeAt(i); 
+		hash = ((hash << 5) - hash) + char; 
+		hash = hash & hash; 
+	} 
+	return String(hash); 
+} 
+
+function generateAuthToken(length) {
+	var result           = '';
+	var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+	var charactersLength = characters.length;
+	for ( var i = 0; i < length; i++ ) {
+	   result += characters.charAt(Math.floor(Math.random() * charactersLength));
+	}
+	return result;
+ }
+
+//need the get request to have the forms show on the website
+ app.get("/signup",function(req,res){
+    res.render("signup.handlebars");
+});
+
+//need the get request to have the forms show on the website
+app.get("/home",function(req,res){
+    res.render("home.handlebars");
+});
+
+
+//Sign up
+app.post("/signup", async (req,res) => {
+	var{ username, first_name , last_name, date_of_birth, password, email } = req.body;
+
+	console.log(password)
+	//turn password to hash version of itself
+	password = stringToHash(password);
+	console.log(password);
+
+	var signUpSqlCommand = `INSERT INTO user (username, first_name, last_name, date_of_birth, password, email)
+	VALUES
+	("`+ username + `","` + first_name + `","` + last_name + `","` + date_of_birth + `","` + password + `","` + email + `");`;
+
+	//if username or email already exist, an error is returned.
+	db.run(signUpSqlCommand,  (err) => {
+		if(err === null)
+		{
+			//res.json({ message: 'signup successful' });
+			//res.redirect("/home");
+			window.location = 'localhost:3000/home'
+		}
+		else {
+			console.log({message:err.toString()});
+			return res.render("signup", { error: err.toString() });
+		}
+	});
+
+});
+
+//if username exists but password doesn't work return incorrect password
+
+//need the get request to have the forms show on the website
+app.get("/login",function(req,res){
+    res.render("login.handlebars");
+});
+
+app.post('/login', async (req, res) => {
+//http://localhost:3000/login?username=felix12&password=felixfelix
+	var{ username, password } = req.body;
+	password = stringToHash(password);
+	var loginString = "SELECT * FROM user WHERE username = '" + username + "' AND password = '" + password + "'";
+
+	//run login command
+	db.get(loginString,  function(err, row){
+		if(err !== null) //if there is an error, print it out
+		{
+			console.log(err);
+			console.log("kiwi");
+		}
+		else //if there is no error check the result
+		{
+			if(typeof row === 'undefined')
+			{
+				res.json({ message: 'login unsuccessful' });
+			}
+			else
+			{
+					authToken = generateAuthToken(10);
+					var authTokenUpdateQuery = "UPDATE user SET authToken = '"+authToken+"' WHERE username = '"+username+"' AND password = '" + password;
+					res.json({ message: 'login successful' });
+			}
+		}
+	});
+});
+
+const setup = async () => {
+	//const db = await dbPromise;
+	createDb();
+	//db.migrate({ force: "last" });
+	app.listen(3000, () => {console.log("Server running on port 3000");});
+};
+
+setup();
